67384b6be94d9c8220e98b2c785e0ebb6530541f
Lip <htq3071243998@gmail.com>
2025-11-17 21:14:40 +0800

    commit1

---
+++ src/main/java/mtc.java
@@ -0,0 +1,12 @@
+import java.net.InetAddress;
+
+public class mtc {
+    public static void main (String[] args )throws Exception {
+        InetAddress ip1=InetAddress.getLocalHost();
+        System.out.println(ip1.getAddress());
+        System.out.println(ip1.getHostName());
+
+
+    }
+}
+
