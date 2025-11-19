4517a4619c5a7e5a0ae5b69ca2c6cc85c0524e16
Lip <htq3071243998@gmail.com>
2025-11-19 11:37:01 +0800

    TCP tongxin2.0

--- src/main/java/TClient.java
+++ src/main/java/TClient.java
@@ -1,2 +1,27 @@
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class TClient {
+    public static void main(String[] args)throws Exception {
+        Socket socket=new Socket("127.0.0.1",6666);
+        OutputStream os=socket.getOutputStream();
+        DataOutputStream dos=new DataOutputStream(os);
+        Scanner sc=new Scanner(System.in);
+
+        while (true) {
+            String msg=sc.nextLine();
+            if (msg.equals("7355608")) {
+                System.out.println("over");
+                dos.close();
+                sc.close();
+                socket.close();
+                break;
+            }
+            dos.writeUTF(msg);
+            dos.flush();
+        }
+    }
 }
--- src/main/java/TServer.java
+++ src/main/java/TServer.java
@@ -1,2 +1,28 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TServer {
+    public static void main(String[] args) throws Exception{
+        ServerSocket Serversocket=new ServerSocket(6666);
+        Socket socket=Serversocket.accept() ;
+        InputStream is=socket.getInputStream();
+        DataInputStream dis=new DataInputStream(is);
+        while (true) {
+            try {
+                String rs = dis.readUTF();
+                System.out.println(rs);
+            } catch (Exception e) {
+                System.out.println(socket.getRemoteSocketAddress()+"over");
+                dis.close();
+                socket.close();
+                break;
+            }
+
+
+        }
+    }
 }
